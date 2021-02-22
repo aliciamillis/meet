2c21ee70e065699e6793363e75a2875ee25029f0
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2021-02-22 11:35:39 -0700

    added /

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -18,11 +18,10 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://aliciamillis.github.io/meet"],
+  redirect_uris: ["https://aliciamillis.github.io/meet/"],
   javascript_origins: [
     "https://aliciamillis.github.io",
-    "http://localhost:3000",
-    "http://127.0.0.1:8080",
+    "http://localhost:3000"
   ],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
