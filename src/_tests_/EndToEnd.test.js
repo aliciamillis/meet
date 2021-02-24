c85fe5b43c3b35c2dc3cd0db42d7e7843546b833
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2021-02-24 12:56:33 -0700

    end to end testing

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -1,21 +1,35 @@
-// import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer';
 
-// beforeAll(async () => {
-//   jest.setTimeout(30000);
-// });
+describe('show/hide an event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
 
-// describe('show/hide an event details', () => {
+  afterAll(() => {
+    browser.close();
+  });
 
-//   test('An event element is collapsed by default', async () => {
-//     const browser = await puppeteer.launch();
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .event__Details');
+    expect(eventDetails).toBeNull();
+  });
 
-//     const page = await browser.newPage();
-//     await page.goto('http://localhost:3000/');
+  test('User can expand an event to see its details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .event__Details');
+    expect(eventDetails).toBeDefined();
+  });
 
-//     await page.waitForSelector('.event');
 
-//     const eventDetails = await page.$('.event .eventList');
-//     expect(eventDetails).toBeNull();
-//     browser.close();
-//   });
-// });
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .event__Details');
+    expect(eventDetails).toBeNull();
+  });
+});
